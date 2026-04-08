fcaf689aee54ab286d5bf20bfdbef0a05de68eed
Penguin1ce <140684560+Penguin1ce@users.noreply.github.com>
2026-04-08 23:38:16 +0800

    Initial Commit

---
+++ buluaichat/buluaichatApp.swift
@@ -0,0 +1,17 @@
+//
+//  buluaichatApp.swift
+//  buluaichat
+//
+//  Created by Zakary on 2026/4/8.
+//
+
+import SwiftUI
+
+@main
+struct buluaichatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
